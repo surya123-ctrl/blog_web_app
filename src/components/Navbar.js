5803f9ddac858dce1fa9d34be3a414be11b5d5f2
Surya Pratap Singh <72143906+surya123-ctrl@users.noreply.github.com>
2023-10-18 20:00:50 +0530

    Initialized

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <ul>
+                <li><a href='https:'>Home</a></li>
+                <li><a href='https:'>Blogs</a></li>
+                <li><a href='https:'>About us</a></li>
+                <li><a href='https:'>Account</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
